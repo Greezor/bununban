4fc15cbcd7068f069c304e030ee380364dfef0ee
Владимир Ольшевский <vov4eg69@gmail.com>
2026-01-26 11:59:56 +0300

    logs: viewport intersection

--- src/frontend/pages/Logs.js
+++ src/frontend/pages/Logs.js
@@ -1,4 +1,4 @@
-import { ref, onActivated } from 'vue'
+import { ref, shallowRef, onActivated, onDeactivated } from 'vue'
 import { css } from '@emotion/css'
 
 import ketchup from '../utils/ketchup'
@@ -7,14 +7,28 @@ import Loader from '../components/Loader'
 
 const style = {
 	page: css`
-		padding: 10px 10px 100px;
+		position: relative;
 		width: 100dvw;
 		height: 100dvh;
 		background: black;
 		white-space: pre;
 		font-family: monospace;
+		font-size: 16px;
+		line-height: 20px;
 		color: #ccc;
 		overflow: auto;
+	`,
+
+	log: css`
+		--top: 0px;
+		padding: 0 10px;
+		position: absolute;
+		top: calc(10px + var(--top));
+		left: 0px;
+
+		&:last-child{
+			padding-bottom: 100px;
+		}
 	`,
 }
 
@@ -22,36 +36,90 @@ export default {
 	setup()
 	{
 		const loading = ref(false);
-		const logs = ref('');
+		const scroll = ref(0);
+		const viewportScroll = ref(0);
+		const viewportHeight = ref(0);
+		const logs = ref([]);
+
+		const logsController = shallowRef(null);
 
 		const loadLogs = async () => {
+			logsController.value?.abort?.();
+			
 			loading.value = true;
+			scroll.value = 0;
+			viewportHeight.value = window.innerHeight;
+			logs.value = [];
+			logsController.value = new AbortController();
 
-			logs.value = '';
+			const response = await ketchup.raw('/api/logs', {
+				signal: logsController.signal,
+			});
 
-			const response = await ketchup.raw('/api/logs');
 			const reader = response.body.getReader();
 			const decoder = new TextDecoder();
 
-			while(true){
+			let tail = '';
+
+			while( !logsController.value.signal.aborted ){
 				const { done, value } = await reader.read();
 				if( done ) break;
 
 				const chunk = decoder.decode(value, { stream: true });
-				logs.value += chunk;
+
+				const lines = (tail + chunk)
+					.split(/\r?\n/);
+
+				tail = lines.pop();
+
+				logs.value.push( ...lines );
+
+				await new Promise(resolve => setTimeout(resolve));
 				
 				loading.value = false;
 			}
+
+			logs.value.push( tail );
 		};
+
+		const rafID = shallowRef(null);
+
+		const loop = () => {
+			rafID.value = requestAnimationFrame(loop);
+			viewportScroll.value = scroll.value;
+		}
 
 		onActivated(() => {
 			loadLogs();
+			loop();
 		});
 
-		return { loading, logs };
+		onDeactivated(() => {
+			logsController.value?.abort?.();
+			cancelAnimationFrame(rafID.value);
+			logs.value = [];
+		});
+
+		return { loading, scroll, viewportScroll, viewportHeight, logs };
 	},
 	template: `
-		<div class="${ style.page }">{{ logs }}</div>
+		<div
+			class="${ style.page }"
+			@scroll="scroll = $event.target.scrollTop">
+			<template v-for="(log, i) in logs">
+				<div
+					v-if="(
+						i === logs.length - 1
+						||
+						( viewportScroll - viewportHeight * 2 <= 20 * i && viewportScroll + viewportHeight * 3 >= 20 * (i + 1) )
+					)"
+					class="${ style.log }"
+					:style="{ '--top': ( 20 * i ) + 'px' }">
+					{{ log }}
+				</div>
+			</template>
+		</div>
+
 		<Loader v-if="loading" />
 	`,
 	components: {
