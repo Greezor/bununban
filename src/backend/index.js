baa3d0ae81852c4de17a07148d1e9f42f4a2b767
Владимир Ольшевский <vov4eg69@gmail.com>
2026-03-11 18:56:09 +0300

    migrate fix

--- src/backend/index.js
+++ src/backend/index.js
@@ -7,6 +7,7 @@ import api from './routes/api'
 import frontend from './routes/frontend'
 
 import zapret from './utils/zapret'
+import migrate from './utils/migrate'
 
 import lists from './stores/lists'
 import lua from './stores/lua'
@@ -14,7 +15,6 @@ import blobs from './stores/blobs'
 import settings from './stores/settings'
 
 import { APPDATA_DIR } from './utils/appdata'
-
 import packageJSON from '../../package.json'
 
 class BackendApp
